fa073f254a0d032bf997bf314e95777c0cfe015d
trapti patel <traptipatel46@gmail.com>
2024-07-22 14:41:08 +0530

    add utils files

---
+++ src/index.js
@@ -0,0 +1,17 @@
+//require('dotenv').config({ path: './env' })// APROACH 1
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 1400, () => {
+            console.log(`server is running at port : ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+    console.log("mongodb connection failed !!!",err)
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve((requestHandler(req,res,next)))
+            .catch((err) => next(err))
+    }
+}
+/*const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    }
+    catch (err) {
+        resizeBy.status(err.code || 500).json({
+            success: false,
+            message:err.message
+        })
+    }
+}
+*/
+export {asyncHandler}
